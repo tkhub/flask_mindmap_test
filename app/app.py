d9b64a737e1d0700b06ba05bad14217905159b87
tkhub <takap.colonel@gmail.com>
2023-05-17 09:44:45 +0900

    add edit and save

--- app/app.py
+++ app/app.py
@@ -23,3 +23,27 @@ def mindmap():
         {"from": "3", "to": "6"}
     ]
     return render_template('mindmap.html', nodes=nodes, edges=edges)
+
+@app.route('/mindmap/edit', methods=['GET', 'POST'])
+def edit_mindmap():
+    if request.method == 'GET':
+        nodes = [
+            {"id": "1", "label": "Node 1"},
+            {"id": "2", "label": "Node 2"},
+            {"id": "3", "label": "Node 3"},
+            {"id": "4", "label": "Node 4"},
+            {"id": "5", "label": "Node 5"},
+            {"id": "6", "label": "Node 6"}
+        ]
+        edges = [
+            {"from": "1", "to": "2"},
+            {"from": "1", "to": "3"},
+            {"from": "2", "to": "4"},
+            {"from": "2", "to": "5"},
+            {"from": "3", "to": "6"}
+        ]
+        return render_template('mindmap_edit.html', nodes=nodes, edges=edges)
+    elif request.method == 'POST':
+        nodes = json.loads(request.form['nodes'])
+        edges = json.loads(request.form['edges'])
+        return render_template('mindmap.html', nodes=nodes, edges=edges)
